3685861219291bec4ecf8d92ec7309141bade7e3
woongryong <woongryong@nexon.co.kr>
2019-08-06 05:14:30 +0000

    Update transactional_state table name in code

--- Orleans.Transactions.PostgreSql/PostgreSqlTransactionalStateOptions.cs
+++ Orleans.Transactions.PostgreSql/PostgreSqlTransactionalStateOptions.cs
@@ -5,7 +5,7 @@ namespace Orleans.Transactions.PostgreSql
     public class PostgreSqlTransactionalStateOptions
     {
         public string ConnectionString { get; set; }
-        public string StateTableName { get; set; } = "transaction_state";
+        public string StateTableName { get; set; } = "transactional_state";
         public string MetadataTableName { get; set; } = "transaction_metadata";
     }
 }
