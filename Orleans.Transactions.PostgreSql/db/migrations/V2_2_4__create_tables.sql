65c38003e4d9795fa4c159353eb84a0a09bdf7e9
woongryong <woongryong@nexon.co.kr>
2019-08-08 20:50:33 +0900

    Add state_type field into DB table

--- Orleans.Transactions.PostgreSql/db/migrations/V2_2_4__create_tables.sql
+++ Orleans.Transactions.PostgreSql/db/migrations/V2_2_4__create_tables.sql
@@ -1,19 +1,20 @@
 CREATE TABLE transaction_metadata
 (
   state_id              VARCHAR(255) NOT NULL,
-  committed_sequence_id INT    NOT NULL,
+  committed_sequence_id INT          NOT NULL,
   etag                  VARCHAR(255) NOT NULL,
-  value                 TEXT  NOT NULL,
+  value                 TEXT         NOT NULL,
   PRIMARY KEY (state_id)
 );
 
 CREATE TABLE transactional_state
 (
-  state_id            VARCHAR(255)    NOT NULL,
-  sequence_id         INT       NOT NULL,
-  transaction_manager TEXT     NOT NULL,
-  value               TEXT     NOT NULL,
-  timestamp           TIMESTAMP NOT NULL,
-  transaction_id      VARCHAR(255)    NOT NULL,
+  state_id            VARCHAR(255)  NOT NULL,
+  sequence_id         INT           NOT NULL,
+  transaction_manager TEXT          NOT NULL,
+  value               TEXT          NOT NULL,
+  timestamp           TIMESTAMP     NOT NULL,
+  transaction_id      VARCHAR(255)  NOT NULL,
+  state_type          VARCHAR(255)  NOT NULL,
   PRIMARY KEY (state_id, sequence_id)
 );
